6f522532e7c843f21dea4aebbf9d65ed32398fcb
Jacob Madden <jacob.madden@gmail.com>
2015-11-21 18:31:03 +0800

    modal register/login

--- models/account.js
+++ models/account.js
@@ -3,8 +3,11 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var Account = new Schema({
+  email: String,
   username: String,
-  password: String
+  password: String,
+  age: String,
+  location: String
 });
 
 Account.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/register', function(req, res) {
 
 router.post('/register', function(req, res) {
   console.log('got /register POST');
-  Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+  Account.register(new Account({ username : req.body.username, email: req.body.email }), req.body.password, function(err, account) {
     if (err) {
       return res.render('register', { account : account });
     }
