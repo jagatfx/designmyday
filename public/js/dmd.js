38533a9bdb27b7ddd26d4bea0ad7588e0b2a87ab
Jacob Madden <jacob.madden@gmail.com>
2015-12-09 17:54:25 +0700

    add local jquery easing and start to break out js in prep for cleanup and use of frontend framework versus ad hoc js

---
+++ public/js/dmd.js
@@ -0,0 +1,23 @@
+
+function fetchActivities() {
+  $.ajax({
+      url: "http://localhost:3000/api/activity?city=Ubud"
+  }).then(function(data) {
+    data.forEach(function(theme, index) {
+      $('div#activities').append(
+        '<a href="'+data[index].link+'" target="_blank">'+
+        '<div class="col-md-3 col-sm-4">'+
+            '<div class="activity">'+
+                '<div class="text">'+
+                  '<h3>'+data[index].activityVerb+'</h3>'+
+                  '<h3>'+data[index].activity+'</h3>'+
+                  '<h3>'+data[index].specificLocation+'</h3>'+
+                  '<p class="votes">'+data[index].targetIntensity+'</p>'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+        '</a'
+      );
+    });
+  });
+}
