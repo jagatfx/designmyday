99395d7fb517b6a44c04df046b8960d096165bcd
Jacob Madden <jacob.madden@gmail.com>
2016-02-21 19:18:21 -0500

    reorg for build

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -39,7 +39,7 @@ var AUTOPREFIXER_BROWSERS = [
   'bb >= 10'
 ];
 
-var DIST = '../server/public/dmd';
+var DIST = '../public/dmd';
 
 var dist = function(subpath) {
   return !subpath ? DIST : path.join(DIST, subpath);
