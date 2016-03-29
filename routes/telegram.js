e3f17590c125f2b6dac0d1744ea9374374de8b7b
Jacob Madden <jacob.madden@gmail.com>
2016-03-28 23:38:37 -0400

    telegram test

--- routes/telegram.js
+++ routes/telegram.js
@@ -2,25 +2,29 @@ var express     = require('express');
 var router      = express.Router();
 var Account     = require('../models/account');
 var Activity    = require('../models/activity');
-var TelegramBot = require('node-telegram-bot-api');
+// var TelegramBot = require('node-telegram-bot-api');
 
-var token = '142335758:AAHd5RxDAXsMgBs7HUsapQrJIKeyaXQlH_4';
-// Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+// var token = '142335758:AAHd5RxDAXsMgBs7HUsapQrJIKeyaXQlH_4';
+// // Setup polling way
+// var bot = new TelegramBot(token, {polling: true});
 
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
-});
+// // Matches /echo [whatever]
+// bot.onText(/\/echo (.+)/, function (msg, match) {
+//   var fromId = msg.from.id;
+//   var resp = match[1];
+//   bot.sendMessage(fromId, resp);
+// });
 
-// Any kind of message
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id
-  var photo = 'http://www.designmyday.co/img/layout.jpg';
-  bot.sendPhoto(chatId, photo, {caption: 'Design My Day'});
+// // Any kind of message
+// bot.on('message', function (msg) {
+//   var chatId = msg.chat.id;
+//   // photo can be: a file path, a stream or a Telegram file_id
+//   var photo = 'http://www.designmyday.co/img/layout.jpg';
+//   bot.sendPhoto(chatId, photo, {caption: 'Design My Day'});
+// });
+
+router.get('/', function (req, res) {
+  res.json({ response : "OK" });
 });
 
 module.exports = router;
