735cc15869b9248d8676468cbcb625faadafd015
Jacob Madden <jacob.madden@gmail.com>
2016-03-29 02:17:21 -0400

    telegram test

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,6 +1,5 @@
 var express     = require('express');
 var router      = express.Router();
-var Account     = require('../models/account');
 var Activity    = require('../models/activity');
 
 
@@ -31,6 +30,30 @@ router.get('/dmdhook', function (req, res) {
   res.json({ok: true});
 });
 
+function sendIdea (back, res) {
+  Activity.count({})
+  .exec(function(err, count) {
+    if (count === 0) {
+      back.text = 'I am all out of ideas!';
+      return res.json(back);
+    }
+    var random = Math.floor(Math.random() * count);
+
+    Activity.findOne({})
+    .skip(random)
+    .exec(function (err, activity) {
+      if (err) {
+        console.error(err);
+        back.text = 'Oops, I had a problem';
+      } else {
+        back.text = activity.activityVerb + ' ' + activity.activity + ' ' + activity.specificLocation +
+          ' (' + activity.city + ', ' + activity.country + ')';
+      }
+      return res.json(back);
+    });
+  });
+}
+
 router.post('/dmdhook', function (req, res) {
   var data = req.body;
   console.log(req.body);
@@ -44,13 +67,48 @@ router.post('/dmdhook', function (req, res) {
   var chat = message.chat;
   var mDate = message.date;
   var text = message.text;
-
+  var cmd;
   var back = {
     ok: true,
     method: 'sendMessage',
     chat_id: chat.id,
-    text: text
+    text: 'Sorry, I do not understand. Try /help for my commands'
+  };
+
+  if (text.startsWith('/')) {
+    cmd = text.substr(1, text.length);
+    switch (cmd) {
+      case 'echo':
+        back.text = text;
+        break;
+      case 'start':
+        back.text = 'hello, '+chat.username+', welcome to Design My Day! Send me a command like /idea';
+        break;
+      case 'help':
+        back.text = 'idea - Get out of your funk with unusual quirky ideas that have worked for others\n' +
+                   'city - Set the city you are interested in\n' +
+                   'vote - Vote on ideas for someone else\n' +
+                   'suggest - Suggest a new idea that people should do\n' +
+                   'feeling - Tell how you are feeling';
+        break;
+      case 'idea':
+        sendIdea(back, res);
+        return;
+      case 'city':
+        back.text = 'I have not yet figured out this feature yet, coming soon!';
+        break;
+      case 'vote':
+        back.text = 'I have not yet figured out this feature yet, coming soon!';
+        break;
+      case 'suggest':
+        back.text = 'I have not yet figured out this feature yet, coming soon!';
+        break;
+      case 'feeling':
+        back.text = 'I have not yet figured out this feature yet, coming soon!';
+        break;
+    }
   }
+
   res.json(back);
 });
 
