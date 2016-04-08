1c7744d8eb5aa8c3ad5b9fb201e36ff208529f5c
Jacob Madden <jacob.madden@gmail.com>
2016-04-07 20:07:18 -0400

    enable service worker cache config

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -281,12 +281,11 @@ gulp.task('serve:dist', ['default'], function() {
 
 // Build production files, the default task
 gulp.task('default', ['clean'], function(cb) {
-  // Uncomment 'cache-config' if you are going to use service workers.
   runSequence(
     ['ensureFiles', 'copy', 'styles'],
     'elements',
     ['images', 'fonts', 'html', 'web-animations'],
-    'vulcanize', // 'cache-config',
+    'vulcanize', 'cache-config',
     cb);
 });
 
